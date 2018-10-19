d52efc48c477e556514f37225e9301a7f4e7b33d
Roger <rgbyford@gmail.com>
2018-10-19 17:04:45 -0500

    keys.js, .env, etc.

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
-console.log('this is loaded');
+//console.log('this is loaded');
 
-exports.keys.spotify = {
-  id: 146bdd87f5604f86ae86228246072a1d,
-  secret: 5e1633fb0ca8445c80d00e685ae7bdce
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
--- liri.js
+++ liri.js
@@ -1,14 +1,11 @@
 require("dotenv").config();
-//require('./keys.js');
+var keys = require('./keys.js');
 var Spotify = require('node-spotify-api');
 var request = require('request');
 var moment = require('moment');
 var fs = require("fs");
 
-var spotify = new Spotify({
-    id: '146bdd87f5604f86ae86228246072a1d',
-    secret: '5e1633fb0ca8445c80d00e685ae7bdce'
-});
+var spotify = new Spotify (keys.spotify);
 
 doStuff(process.argv[2], process.argv[3]);
 
